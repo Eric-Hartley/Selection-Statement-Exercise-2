95055c8a917b5bc3512c4c08912164601dcaafae
Eric Hartley <spiffydude0@gmail.com>
2024-08-06 15:11:15 -0500

    exercise complete

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite subject?");
+            var favSubject = Console.ReadLine().ToLower();
+
+            switch (favSubject)
+            {
+                case "math":
+                    Console.WriteLine($"{favSubject}? Nerd...");
+                    break;
+                case "science":
+                    Console.WriteLine($"{favSubject}? Okay Einstein");
+                    break;
+                case "english":
+                    Console.WriteLine($"{favSubject}? Snob");
+                    break;
+                case "history":
+                    Console.WriteLine($"{favSubject}? Good for you");
+                    break;
+                case "philosophy":
+                    Console.WriteLine($"{favSubject}? Alright Pluto");
+                    break;
+                default :
+                    Console.WriteLine("What subject is that?");
+                        break;
+
+
+
+
+            }
         }
     }
 }
